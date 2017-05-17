6e5eccc199bac3a0efbcdab4367d5a3d13fec9b4
Enver Pezić <enver_pez@outlook.com>
2017-05-17 07:16:21 +0200

    Autentifikacija NotificationModule

--- NWTProjekatJukebox/NotificationModule/src/main/java/NotificationService/NotificationApplication.java
+++ NWTProjekatJukebox/NotificationModule/src/main/java/NotificationService/NotificationApplication.java
@@ -3,6 +3,7 @@ package NotificationService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.ServiceInstance;
@@ -10,24 +11,38 @@ import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 /**
- * Created by Šahin on 18.3.2017.
+ * Created by Enver on 18.3.2017.
  */
 @PropertySource({"classpath:application.properties"})
 @EnableDiscoveryClient
 @Configuration
 @SpringBootApplication
+@RestController
 public class NotificationApplication {
+    @Value("${user.role}")
+    private String role;
+
     public static void main(String[] args) {
         Logger log= LoggerFactory.getLogger(NotificationApplication.class);
         SpringApplication.run(NotificationApplication.class, args);
         log.info("NotificationService.Application is started");
+    }
+
+    @RequestMapping(
+            value = "/whoami/{username}",
+            method = RequestMethod.GET,
+            produces = MediaType.TEXT_PLAIN_VALUE)
+    public String whoami(@PathVariable("username") String username) {
+        return String.format("Hello! You're %s and you'll become a(n) %s...\n", username, role);
     }
 }
 
